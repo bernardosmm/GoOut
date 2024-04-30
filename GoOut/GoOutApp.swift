8ae0855d6602a1e00240e819611bd0fb0326acb9
Bernardo Santos <130390914+bernardosmm@users.noreply.github.com>
2024-04-30 08:36:05 -0300

    Initial Commit

---
+++ GoOut/GoOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoOutApp.swift
+//  GoOut
+//
+//  Created by User on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct GoOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
