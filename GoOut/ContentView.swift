b4f1492e5bad54e36f32ac7ace2859fd7d85820b
joao pedro <joao.pedro73210@gmail.com>
2024-05-08 11:45:29 -0300

    i try so hard

--- GoOut/ContentView.swift
+++ GoOut/ContentView.swift
@@ -4,11 +4,16 @@ import MapKit  // Importando biblioteca do mapa
 
 
 struct ContentView: View {
-    @State var locations = [
-        Locations(name: "Tabernas", coordinate: CLLocationCoordinate2D(latitude: -3.744751, longitude: -38.537488)),
+    @State public var locations = [
         Locations(name: "Praia Nautico", coordinate: CLLocationCoordinate2D(latitude: -3.7246985883163584, longitude: -38.491181818998) )
     ]
     
+    func createMarker(){
+        let locat = Locations(name: "Academy", coordinate: CLLocationCoordinate2D(latitude: -3.744704, longitude: -38.536394))
+        self.locations.append(locat)
+    }
+    
+    //TODO: criar funcao para gerar uma location toda vez que o usuario preencher o form
     var body: some View {
 //        @State var locations = [
 //            Locations(name: "Tabernas", coordinate: CLLocationCoordinate2D(latitude: -3.744751, longitude: -38.537488)),
