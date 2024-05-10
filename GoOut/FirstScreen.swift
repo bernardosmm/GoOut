2f26695d6846e7e4cac15d83ab75229f1df30aab
Leticia Bezerra <leticiabezerrauy@gmail.com>
2024-05-10 09:15:46 -0300

    Search bar finalizada

--- GoOut/FirstScreen.swift
+++ GoOut/FirstScreen.swift
@@ -9,108 +9,33 @@ import Foundation
 import SwiftUI
 import MapKit  // Importando biblioteca do mapa
 
-//struct ContentView: View {
-//
-//    @State private var mostrarSheet = false
-//
-//    var body: some View {
-//        ZStack {
-//            Map()
-//                .mapStyle(.hybrid(elevation: .flat))
-//            Button("Mostrar sheet view") {
-//                mostrarSheet = true
-//            }
-//            .fullScreenCover(isPresented: $mostrarSheet) {
-////            .sheet(isPresented: $mostrarSheet) {
-//            FirstScreen() // Instancia do Componente
-//            }
-//            .buttonStyle(.borderedProminent)
-//        }
-//    }
-//}
-
-//struct SheetView: View {
-//    @Environment(\.dismiss) var dismiss
-//
-//    var body: some View {
-//        ZStack {
-//            Rectangle()
-//                .fill(.black)
-//                .ignoresSafeArea(.all)
-//            VStack {
-//                Text("Esta é minha Sheet View!")
-//                    .foregroundColor(.white)
-//                Button("Sair da sheet view") {
-//                    dismiss()
-//                }
-//            }
-//        }
-//    }
-//}
-
 struct FirstScreen: View {
     
     @State var flag: Bool = false
-    @State var detents: PresentationDetent = .height(100)
-//    @State var searchText = ""
-//    @State var seacrhIsActive = false
+    @State var detents: PresentationDetent = .height(60)
+    @Binding var searchText: String
     
     var body: some View {
         NavigationStack {
+            VStack {
+                SearchBar(searchText: $searchText)
+                List {
+                    if self.detents != .height(60) {
+                    }
+                    //                else {
+                    //                    Text("B")
+                    //                }
+                }
+                .listStyle(.plain)
+            }
         }
-        .searchable(text: .constant("Procure por um evento.."))
         .presentationDetents([.height(60), .medium], selection: $detents)
     }
 }
-//        .searchable(text: .constant("search here"))
-//        .presentationDetents([.height(90), .medium], selection: $detents)
-    
-    //        VStack {
-    ////            ScrollView {
-    ////                ForEach(0..<15) { _ in
-    ////                    Button {
-    ////                        flag = !flag
-    ////                    } label: {
-    ////                        VStack(spacing: 10) {
-    ////                            Image(systemName: "swift")
-    ////                            Text("Swift is cool")
-    ////                        }
-    ////                        .padding()
-    ////                        .background {
-    ////                            RoundedRectangle(cornerRadius: 11)
-    ////                                .fill(detents == .medium ? .orange : .blue)
-    ////                        }
-    ////                    }
-    ////                    .foregroundStyle(.white)
-    ////                }
-//                }.sheet(isPresented: $flag, content: { // cifrao é um Binding pra um State. É parte de como um property wrapper funciona. @State é um property wrapper.
-//                    NavigationStack {
-//                        Text("Aaaa")
-//                            .navigationTitle("AAAA")
-//                            .navigationBarTitleDisplayMode(.inline)
-//                    }
-//                    .searchable(text: .constant("search here"))
-//                    .presentationDetents([.height(90), .medium], selection: $detents)
-//                })
-//            }
 
 #Preview {
     ContentView() // View do Bernardo com Pedro
         .sheet(isPresented: .constant(true)) {
-            FirstScreen() // Minha View
-                .presentationDetents([.height(60), .medium, .large])
+            FirstScreen(searchText: .constant("")) // Minha View
         }
 }
-
-//struct PreviewMapView: View {
-//    var body: some View {
-//        Map(initialPosition: .region(region))
-//    }
-//    
-//    private var region: MKCoordinateRegion {
-//        MKCoordinateRegion(
-//            center: CLLocationCoordinate2D(latitude: -3.7442604368944856, longitude: -38.53568772387148),
-//            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-//        )
-//    }
-
--- GoOut/SearchBar.swift
+++ GoOut/SearchBar.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import SwiftUI
+import MapKit
+
+struct SearchBar: View {
+    
+    @Binding var searchText: String
+        
+    @FocusState var isFocused: Bool
+    
+    var body: some View {
+        HStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(.gray)
+                    .padding(
+                        EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 0)
+                    )
+                TextField("Pesquise um evento...", text: $searchText)
+                    .focused($isFocused)
+            }
+            .frame(maxWidth: .infinity, minHeight: 35)
+            .background(Color(white: 0.9))
+            .cornerRadius(10)
+            
+            if isFocused {
+                Button("Cancel") {
+                    isFocused = false
+                }
+                .transition(.move(edge: .trailing).combined(with: .opacity))
+            }
+        }
+        .animation(.default, value: isFocused)
+        .frame(height: 35)
+        .padding(.top, 30)
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    ContentView() // View do Bernardo com Pedro
+        .sheet(isPresented: .constant(true)) {
+            FirstScreen(searchText: .constant("")) // Minha View
+        }
+}
