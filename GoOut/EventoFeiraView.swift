068697d3f164361756cfb3867bd61f25d3074e07
Princesa-Trindade <emaildaprincesona@gmail.com>
2024-05-13 10:32:03 -0300

    Telas de eventos criadas.

---
+++ GoOut/EventoFeiraView.swift
@@ -0,0 +1,18 @@
+//
+//  EventoFeiraView.swift
+//  GoOut
+//
+//  Created by Princesa Trindade Rebouças Esmeraldo Nunes on 13/05/24.
+//
+
+import SwiftUI
+
+struct EventoFeiraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventoFeiraView()
+}
---
+++ GoOut/EventoTrilhaView.swift
@@ -0,0 +1,18 @@
+//
+//  EventoTrilhaView.swift
+//  GoOut
+//
+//  Created by Princesa Trindade Rebouças Esmeraldo Nunes on 13/05/24.
+//
+
+import SwiftUI
+
+struct EventoTrilhaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventoTrilhaView()
+}
